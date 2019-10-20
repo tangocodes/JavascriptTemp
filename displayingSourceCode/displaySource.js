9602168a957e1357ee177ca886d2b1444c530431
Sai Sumanth <39739036+Mr404Found@users.noreply.github.com>
2019-10-20 15:24:33 +0530

    Update displaySource.js

--- displayingSourceCode/displaySource.js
+++ displayingSourceCode/displaySource.js
@@ -2,3 +2,7 @@
 
 document.documentElement.innerHTML =
   "<pre>" + document.documentElement.innerHTML.replace(/</g, "&lt;") + "</pre>";
+
+const addPara=document.createElement('p')
+        addPara.textContent="Passwords Didn't match"
+        document.querySelector('div').appendChild(addPara)
